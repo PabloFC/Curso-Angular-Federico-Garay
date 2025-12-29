2000e282088cfb3bdc3e78656b2d5f466cc2894f
PabloFC <pablofc_68@hotmail.com>
2025-12-29 09:09:33 +0100

    first commit

---
+++ 01-proyecto-inicial/01-proyecto-inicial/src/app/usuario/usuario.component.ts
@@ -0,0 +1,24 @@
+import { Usuario } from './usuario.model';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuario.component.html',
+  styleUrl: './usuario.component.css',
+})
+export class UsuarioComponent {
+  @Input({ required: true }) usuario!: Usuario;
+  @Input({ required: true }) seleccionado!: boolean;
+
+  @Output() seleccion = new EventEmitter<string>();
+
+  get rutaImagen() {
+    return 'assets/usuarios/' + this.usuario.avatar;
+  }
+
+  alSeleccionarUsuario() {
+    this.seleccion.emit(this.usuario.id);
+  }
+}
