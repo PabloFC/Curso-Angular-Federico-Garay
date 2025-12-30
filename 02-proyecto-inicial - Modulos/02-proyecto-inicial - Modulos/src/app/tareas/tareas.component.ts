314c99dfda8540e1eb56dbed0c2adc0ec31e73fe
PabloFC <pablofc_68@hotmail.com>
2025-12-30 13:19:16 +0100

    setting 02-project-modules

---
+++ 02-proyecto-inicial - Modulos/02-proyecto-inicial - Modulos/src/app/tareas/tareas.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+import { TareaComponent } from "./tarea/tarea.component";
+import { NuevaTareaComponent } from "./nueva-tarea/nueva-tarea.component";
+import { type NuevaTareaInfo } from './tarea/tarea.model';
+import { TareasService } from './tareas.service';
+
+@Component({
+  selector: 'app-tareas',
+  standalone: true,
+  imports: [TareaComponent, NuevaTareaComponent],
+  templateUrl: './tareas.component.html',
+  styleUrl: './tareas.component.css'
+})
+export class TareasComponent {
+  @Input({required: true}) idUsuario!: string;
+  @Input({required: true}) nombre!: string;
+  estaAgregandoTareaNueva = false;
+
+
+  constructor(private tareasService: TareasService) {}
+
+  get tareasUsuarioSeleccionado() {
+    return this.tareasService.obtenerTareasDeUsuario(this.idUsuario);
+  }
+
+  alIniciarNuevaTarea() {
+    this.estaAgregandoTareaNueva = true;
+  }
+
+  alCerrarTareaNueva() {
+    this.estaAgregandoTareaNueva = false;
+  }
+}
