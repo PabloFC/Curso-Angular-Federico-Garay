e26d070965fafbc60e7ca9bf0584568366e8bb81
PabloFC <pablofc_68@hotmail.com>
2026-01-02 09:33:02 +0100

    devtools angular

--- 04-proyecto-para-debugging/04-proyecto-para-debugging/src/app/tareas/tareas.service.ts
+++ 04-proyecto-para-debugging/04-proyecto-para-debugging/src/app/tareas/tareas.service.ts
@@ -6,26 +6,28 @@ import { type NuevaTareaInfo } from './tarea/tarea.model';
 export class TareasService {
   private tareas = [
     {
-          id: 't1',
-          idUsuario: 'u1',
-          titulo: 'Dominar Angular',
-          resumen: 'Aprender todas las características básicas y avanzadas de Angular y saber c+omo apñicarlas',
-          expira: '2025-01-01',
-        },
-        {
-          id: 't2',
-          idUsuario: 'u3',
-          titulo: 'Crear el primer prototipo',
-          resumen: 'Crear el primer prototipo del sitio web de la tienda',
-          expira: '2025-11-05',
-        },
-        {
-          id: 't3',
-          idUsuario: 'u3',
-          titulo: 'Preparar la plantilla del carrito',
-          resumen: 'Preparar y describir una plantilla de carrito de compras de la tienda online',
-          expira: '2025-05-16',
-        },
+      id: 't1',
+      idUsuario: 'u1',
+      titulo: 'Dominar Angular',
+      resumen:
+        'Aprender todas las características básicas y avanzadas de Angular y saber c+omo apñicarlas',
+      expira: '2025-01-01',
+    },
+    {
+      id: 't2',
+      idUsuario: 'u3',
+      titulo: 'Crear el primer prototipo',
+      resumen: 'Crear el primer prototipo del sitio web de la tienda',
+      expira: '2025-11-05',
+    },
+    {
+      id: 't3',
+      idUsuario: 'u3',
+      titulo: 'Preparar la plantilla del carrito',
+      resumen:
+        'Preparar y describir una plantilla de carrito de compras de la tienda online',
+      expira: '2025-05-16',
+    },
   ];
 
   constructor() {
@@ -43,7 +45,7 @@ export class TareasService {
   agregarTarea(infoDeTarea: NuevaTareaInfo, idUsuario: string) {
     this.tareas.unshift({
       id: new Date().getTime().toString(),
-      idUsuario: '',
+      idUsuario: idUsuario,
       titulo: infoDeTarea.titulo,
       resumen: infoDeTarea.resumen,
       expira: infoDeTarea.fecha,
