a8b04599b764328e6f96dfa0bcd476f9ca38473f
PabloFC <pablofc_68@hotmail.com>
2026-01-05 09:14:37 +0100

    form added

--- 05-dashboard/05-dashboard/src/app/dashboard/registros/registros.component.ts
+++ 05-dashboard/05-dashboard/src/app/dashboard/registros/registros.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { NuevoRegistroComponent } from './nuevo-registro/nuevo-registro.component';
 
 @Component({
   selector: 'app-registros',
   standalone: true,
-  imports: [],
+  imports: [NuevoRegistroComponent],
   templateUrl: './registros.component.html',
-  styleUrl: './registros.component.css'
+  styleUrl: './registros.component.css',
 })
-export class RegistrosComponent {
-
-}
+export class RegistrosComponent {}
