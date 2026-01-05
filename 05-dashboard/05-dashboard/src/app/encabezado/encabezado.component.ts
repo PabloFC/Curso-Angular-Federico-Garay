c9a60709402dd9d4c38c46422b46f8ab8fbbb31b
PabloFC <pablofc_68@hotmail.com>
2026-01-05 09:31:25 +0100

    button component

--- 05-dashboard/05-dashboard/src/app/encabezado/encabezado.component.ts
+++ 05-dashboard/05-dashboard/src/app/encabezado/encabezado.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { BotonComponent } from '../compartida/boton/boton.component';
 
 @Component({
   selector: 'app-encabezado',
   standalone: true,
-  imports: [],
+  imports: [BotonComponent],
   templateUrl: './encabezado.component.html',
-  styleUrl: './encabezado.component.css'
+  styleUrl: './encabezado.component.css',
 })
-export class EncabezadoComponent {
-
-}
+export class EncabezadoComponent {}
