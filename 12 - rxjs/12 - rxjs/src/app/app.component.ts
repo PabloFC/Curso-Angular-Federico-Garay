436d815b27c59124817891cf41c14a6ed4c12340
PabloFC <pablofc_68@hotmail.com>
2026-01-19 13:20:16 +0100

    convertir observables a signals

--- 12 - rxjs/12 - rxjs/src/app/app.component.ts
+++ 12 - rxjs/12 - rxjs/src/app/app.component.ts
@@ -1,5 +1,13 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import {
+  Component,
+  DestroyRef,
+  effect,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-raiz',
@@ -8,14 +16,38 @@ import { interval } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
+  constructor() {
+    // effect(() => {
+    //   console.log(`Boton clickeado ${this.cuentaClicks()} veces`);
+    // });
+  }
+  cuentaClicks = signal(0);
+  cuentaClicks$ = toObservable(this.cuentaClicks);
+  intervalo$ = interval(1000);
+  signalIntevalo = toSignal(this.intervalo$, {
+    initialValue: 0,
+    manualCleanup: true,
+  });
 
   ngOnInit(): void {
-    const subscripcion = interval(1000).subscribe({
-      next: (valor) => console.log(valor),
+    const subscripcion = this.cuentaClicks$.subscribe({
+      next: (valor) =>
+        console.log(`Boton clickeado ${this.cuentaClicks()} veces`),
     });
-
     this.destroyRef.onDestroy(() => {
       subscripcion.unsubscribe();
     });
+    // const subscripcion = interval(1000)
+    //   .pipe(map((valor) => valor * 2))
+    //   .subscribe({
+    //     next: (valor) => console.log(valor),
+    //   });
+    // this.destroyRef.onDestroy(() => {
+    //   subscripcion.unsubscribe();
+    // });
+  }
+
+  alClickear() {
+    this.cuentaClicks.update((cuentaAnterior) => cuentaAnterior + 1);
   }
 }
