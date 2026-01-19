704c11a8e6327331074b8cbd801de889265c262b
PabloFC <pablofc_68@hotmail.com>
2026-01-19 17:59:16 +0100

    Observable personalizado

--- 12 - rxjs/12 - rxjs/src/app/app.component.ts
+++ 12 - rxjs/12 - rxjs/src/app/app.component.ts
@@ -7,7 +7,7 @@ import {
   signal,
 } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { interval, map } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-raiz',
@@ -15,6 +15,21 @@ import { interval, map } from 'rxjs';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
+  intervaloPersonal$ = new Observable((subscriptor) => {
+    let contador = 0;
+    const intervalo = setInterval(() => {
+      if (contador > 3) {
+        clearInterval(intervalo);
+        subscriptor.complete();
+        return;
+      }
+      console.log('emitiendo nuevo valor');
+      subscriptor.next({
+        mensaje: 'Nuevo Valor',
+      });
+      contador++;
+    }, 2000);
+  });
   private destroyRef = inject(DestroyRef);
   constructor() {
     // effect(() => {
@@ -30,6 +45,10 @@ export class AppComponent implements OnInit {
   });
 
   ngOnInit(): void {
+    this.intervaloPersonal$.subscribe({
+      next: (valor) => console.log(valor),
+      complete: () => console.log('completada!'),
+    });
     const subscripcion = this.cuentaClicks$.subscribe({
       next: (valor) =>
         console.log(`Boton clickeado ${this.cuentaClicks()} veces`),
