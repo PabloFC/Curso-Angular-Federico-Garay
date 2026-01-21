dd473ef10e6b78f8f59412b8e570b30768702264
PabloFC <pablofc_68@hotmail.com>
2026-01-21 11:00:16 +0100

    obtener mas datos

--- 12-cliente-http/12-cliente-http/src/app/lugares/lugares-usuario/lugares-usuario.component.ts
+++ 12-cliente-http/12-cliente-http/src/app/lugares/lugares-usuario/lugares-usuario.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, signal, OnInit } from '@angular/core';
 
 import { Lugar } from '../lugar.model';
 import { ContenedorLugaresComponent } from '../contenedor-lugares/contenedor-lugares.component';
 import { LugaresComponent } from '../lugares.component';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-lugares-usuario',
@@ -11,6 +13,40 @@ import { LugaresComponent } from '../lugares.component';
   styleUrl: './lugares-usuario.component.css',
   imports: [ContenedorLugaresComponent, LugaresComponent],
 })
-export class LugaresUsuarioComponent {
+export class LugaresUsuarioComponent implements OnInit {
   lugares = signal<Lugar[] | undefined>(undefined);
+  estaRecibiendo = signal(false);
+  error = signal('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.estaRecibiendo.set(true);
+    const subscripcion = this.httpClient
+      .get<{ lugares: Lugar[] }>('http://localhost:3000/lugares-usuario')
+      .pipe(
+        map((datosrespuesta) => datosrespuesta.lugares),
+        catchError((error) => {
+          console.log(error.message);
+          return throwError(
+            () => new Error('Algo salió mal al obtener tus sitios favoritos'),
+          );
+        }),
+      )
+      .subscribe({
+        next: (lugares) => {
+          this.lugares.set(lugares);
+        },
+        complete: () => {
+          this.estaRecibiendo.set(false);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscripcion.unsubscribe();
+    });
+  }
 }
