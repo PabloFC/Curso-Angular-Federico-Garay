7b64ba5d4ad7df8486164e4d8ed4558873279aeb
PabloFC <pablofc_68@hotmail.com>
2026-01-23 12:50:23 +0100

    . Interactuando con el Objeto Subyacente del Formulario en el Componente

--- 14-formularios/src/app/auth/login/login.component.ts
+++ 14-formularios/src/app/auth/login/login.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +16,24 @@ import { FormsModule, NgForm } from '@angular/forms';
   imports: [FormsModule],
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyref = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const subscripcion = this.form()
+        .valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (valor) =>
+            window.localStorage.setItem(
+              'form-login-guardado',
+              JSON.stringify({ email: valor.email }),
+            ),
+        });
+      this.destroyref.onDestroy(() => subscripcion?.unsubscribe());
+    });
+  }
+
   alEnviar(datosForm: NgForm) {
     if (datosForm.form.invalid) {
       return;
@@ -16,5 +41,7 @@ export class LoginComponent {
     const emailIngresado = datosForm.form.value.email;
     const contrasenaIngresado = datosForm.form.value.contrasena;
     console.log(datosForm);
+
+    datosForm.form.reset();
   }
 }
